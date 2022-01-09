43c965f193473ec2b0e2b7839d71a2b7f98db21f
satma0745 <catman0745@gmail.com>
2022-01-09 15:30:15 +0300

    Add DTO validation

--- DataTransferObjects/CreateMeetupDto.cs
+++ DataTransferObjects/CreateMeetupDto.cs
@@ -1,15 +1,22 @@
 ﻿namespace Meetups.DataTransferObjects;
 
 using System;
+using System.ComponentModel.DataAnnotations;
 
 public class CreateMeetupDto
 {
+    [Required]
+    [MaxLength(100)]
     public string Topic { get; set; }
     
+    [Required]
+    [MaxLength(75)]
     public string Place { get; set; }
     
     /// <summary> Duration in minutes. </summary>
+    [Range(120, 720)]
     public int Duration { get; set; }
     
+    [Required]
     public DateTime StartTime { get; set; }
 }
