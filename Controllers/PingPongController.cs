a847caef79b55092337793f93f3444bcf5b6e5a8
satma0745 <catman0745@gmail.com>
2022-01-09 14:59:53 +0300

    Clenup

---
+++ Controllers/PingPongController.cs
@@ -0,0 +1,11 @@
+﻿namespace Meetups.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/ping-pong")]
+public class HelloWorldController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Ping() => Ok("Pong!");
+}
