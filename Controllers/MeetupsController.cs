fd1f22059a2c9921a784de83921dad0f560d6769
satma0745 <catman0745@gmail.com>
2022-01-09 15:09:56 +0300

    Replace ping-pong with meetups

---
+++ Controllers/MeetupsController.cs
@@ -0,0 +1,37 @@
+﻿namespace Meetups.Controllers;
+
+using System;
+using System.Collections.Generic;
+using Meetups.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("/api/meetups")]
+public class MeetupsController : ControllerBase
+{
+    private static readonly List<Meetup> Meetups = new();
+
+    [HttpGet]
+    public IActionResult GetAllMeetups() => Ok(Meetups);
+
+    /// <example>
+    /// {
+    ///   "topic": "Microsoft naming issues.",
+    ///   "place": "Oslo",
+    ///   "duration": 180,
+    ///   "startTime": "2022-01-09T12:00:00.000Z"
+    /// }
+    /// </example>
+    [HttpPost]
+    public IActionResult RegisterNewMeetup(Meetup meetup)
+    {
+        // set id if requester did not bother to
+        if (meetup.Id == Guid.Empty)
+        {
+            meetup.Id = Guid.NewGuid();
+        }
+        
+        Meetups.Add(meetup);
+        return Ok();
+    }
+}
---
+++ Entities/Meetup.cs
@@ -0,0 +1,17 @@
+﻿namespace Meetups.Entities;
+
+using System;
+
+public class Meetup
+{
+    public Guid Id { get; set; }
+    
+    public string Topic { get; set; }
+    
+    public string Place { get; set; }
+    
+    /// <summary> Duration in minutes. </summary>
+    public int Duration { get; set; }
+    
+    public DateTime StartTime { get; set; }
+}
