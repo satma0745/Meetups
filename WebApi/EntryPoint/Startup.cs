5791891214e16e83a4a0613590d493cc6280ee20
satma0745 <catman0745@gmail.com>
2022-02-05 17:46:21 +0300

    Refactor DI & Middlewares configuration

---
+++ WebApi/EntryPoint/Startup.cs
@@ -0,0 +1,35 @@
+﻿namespace Meetups.WebApi.EntryPoint;
+
+using System.Reflection;
+using Meetups.Features.Shared;
+using Meetups.Persistence.Context;
+using Meetups.WebApi.Auth;
+using Meetups.WebApi.Swagger;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+internal class Startup
+{
+    private readonly IConfiguration configuration;
+
+    public Startup(IConfiguration configuration) =>
+        this.configuration = configuration;
+
+    public void ConfigureServices(IServiceCollection services) =>
+        services
+            .AddApplicationContext()
+            .AddScoped<TokenHelper>()
+            .AddAutoMapper(Assembly.GetExecutingAssembly())
+            .AddAuth(configuration)
+            .AddSwaggerDocs()
+            .AddControllers();
+
+    public static void Configure(IApplicationBuilder application, IWebHostEnvironment environment) =>
+        application
+            .UseSwaggerDocs(environment)
+            .UseRouting()
+            .UseAuth()
+            .UseEndpoints(endpoints => endpoints.MapControllers());
+}
