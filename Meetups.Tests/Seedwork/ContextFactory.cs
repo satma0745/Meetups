39a2020f502193784c15b09660ca52e55f223370
satma0745 <catman0745@gmail.com>
2022-02-27 21:38:35 +0300

    M1: Add testing infrastructure.

---
+++ Meetups.Tests/Seedwork/ContextFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace Meetups.Tests.Seedwork;
+
+using Meetups.Application.Modules.Persistence;
+using Meetups.Application.Modules.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+internal static class ContextFactory
+{
+    public static IApplicationContext InMemory()
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+        optionsBuilder.UseInMemoryDatabase("In-memory DB");
+
+        var options = optionsBuilder.Options;
+        var context = new ApplicationContext(options);
+
+        return context;
+    }
+}
