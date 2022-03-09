a30573b96a6d56defc0ac41511d09a989052bc88
satma0745 <catman0745@gmail.com>
2022-03-09 21:58:09 +0300

    Add gulp script to automate creation of the '/migrations.sql' file

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+﻿const { dest, pipe, src } = require('gulp')
+const concat = require('gulp-concat')
+
+const buildMigrations = () =>
+  src('./Meetups.Application.Modules.Persistence/Migrations/*.sql')
+    .pipe(concat('migrations.sql', { newLine: '\n\n' }))
+    .pipe(dest('./'))
+
+exports.buildMigrations = buildMigrations
--- migrations.sql
+++ migrations.sql
@@ -1,4 +1,4 @@
-﻿START TRANSACTION;
+START TRANSACTION;
 
 DO $EF$
 BEGIN
@@ -346,15 +346,15 @@ COMMIT;
 START TRANSACTION;
 
 DO $EF$
-    BEGIN
-        IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20220222200452_FixMeetupOrganizerIdIndexName') THEN
-
-            ALTER INDEX "IX_meetups_organizer_id" RENAME TO ix_meetups_organizer_id;
-
-            INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
-            VALUES ('20220222200452_FixMeetupOrganizerIdIndexName', '6.0.1');
-
-        END IF;
-    END $EF$;
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20220222200452_FixMeetupOrganizerIdIndexName') THEN
+        
+        ALTER INDEX "IX_meetups_organizer_id" RENAME TO ix_meetups_organizer_id;
+        
+        INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+        VALUES ('20220222200452_FixMeetupOrganizerIdIndexName', '6.0.1');
+        
+    END IF;
+END $EF$;
 
 COMMIT;
